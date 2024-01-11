f1d5c29940fe284edaa238e0aa25e2fa06a53bf0
Oleg Grech <oleg.grech.19@um.edu.mt>
2024-01-11 21:40:04 +0100

    refactor: minor refactoring

--- app/api/food/route.ts
+++ app/api/food/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
 import { collection, getDocs } from "firebase/firestore";
-import { firestoreDb } from "@/app/firebase";
+import { db } from "@/app/firebase";
 
 export async function GET() {
-  const querySnapshot = await getDocs(collection(firestoreDb, "food"));
+  const querySnapshot = await getDocs(collection(db, "food"));
   return NextResponse.json({
     result: querySnapshot.docs.map((doc) => doc.data()),
   });
--- app/components/preferences.tsx
+++ app/components/preferences.tsx
@@ -1,10 +1,16 @@
-import { FC, useState, useEffect } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { FC, useEffect, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-import { doc, getDoc, updateDoc } from 'firebase/firestore';
-import { db, auth } from '../firebase';
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { auth, db } from "../firebase";
 
-import { Snackbar, MenuItem, Select, InputLabel, FormControl } from '@mui/material';
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Snackbar,
+} from "@mui/material";
 
 interface FormValues {
   prefBreakfast: string;
@@ -13,137 +19,157 @@ interface FormValues {
 }
 
 const Preferences: FC = () => {
-    const { register, handleSubmit } = useForm<FormValues>();
-    const [selectedPreferences, setSelectedPreferences] = useState<string[]>([]);
-    const [temporaryPreferences, setTemporaryPreferences] = useState<FormValues>({
-      prefBreakfast: '',
-      prefLunch: '',
-      prefDinner: '',
-    });
-    const [showSelectedPreferences, setShowSelectedPreferences] = useState(false);
-    const [error, setError] = useState<string | null>(null);
-    const [snackbarOpen, setSnackbarOpen] = useState(false);
-    const [loading, setLoading] = useState(true);
-  
-    const handleSnackbarClose = () => {
-      setSnackbarOpen(false);
-    };
-  
-    const handlePreferenceChange = (meal: string, value: string) => {
-      setTemporaryPreferences((prevTemporaryPreferences) => ({
-        ...prevTemporaryPreferences,
-        [meal]: value,
-      }));
-    };
-  
-    const handleChangePreferences: SubmitHandler<FormValues> = async (data) => {
-      try {
-        const uid = auth.currentUser?.uid;
-  
-        if (uid) {
-          const userDocRef = doc(db, 'users', uid);
-  
-          await updateDoc(userDocRef, {
-            prefBreakfast: data.prefBreakfast,
-            prefLunch: data.prefLunch,
-            prefDinner: data.prefDinner,
-          });
-  
-          setSelectedPreferences(Object.values(data));
-          setShowSelectedPreferences(true);
-  
-          setError('User preferences updated.');
-          setSnackbarOpen(true);
-        }
-      } catch (error) {
-        setError('Error updating preferences: ' + error.message);
+  const { register, handleSubmit } = useForm<FormValues>();
+  const [selectedPreferences, setSelectedPreferences] = useState<string[]>([]);
+  const [temporaryPreferences, setTemporaryPreferences] = useState<FormValues>({
+    prefBreakfast: "",
+    prefLunch: "",
+    prefDinner: "",
+  });
+  const [showSelectedPreferences, setShowSelectedPreferences] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
+  const handlePreferenceChange = (meal: string, value: string) => {
+    setTemporaryPreferences((prevTemporaryPreferences) => ({
+      ...prevTemporaryPreferences,
+      [meal]: value,
+    }));
+  };
+
+  const handleChangePreferences: SubmitHandler<FormValues> = async (data) => {
+    try {
+      const uid = auth.currentUser?.uid;
+
+      if (uid) {
+        const userDocRef = doc(db, "users", uid);
+
+        await updateDoc(userDocRef, {
+          prefBreakfast: data.prefBreakfast,
+          prefLunch: data.prefLunch,
+          prefDinner: data.prefDinner,
+        });
+
+        setSelectedPreferences(Object.values(data));
+        setShowSelectedPreferences(true);
+
+        setError("User preferences updated.");
         setSnackbarOpen(true);
       }
-    };
-  
-    useEffect(() => {
-      const fetchUserPreferences = async () => {
-        try {
-          const uid = auth.currentUser?.uid;
-  
-          if (uid) {
-            const userDocRef = doc(db, 'users', uid);
-            const userDocSnapshot = await getDoc(userDocRef);
-  
-            if (userDocSnapshot.exists()) {
-              const userData = userDocSnapshot.data();
-              const { prefBreakfast, prefLunch, prefDinner } = userData || {};
-  
-              setTemporaryPreferences({
-                prefBreakfast: prefBreakfast || '',
-                prefLunch: prefLunch || '',
-                prefDinner: prefDinner || '',
-              });
-              setSelectedPreferences([prefBreakfast, prefLunch, prefDinner]);
-              setShowSelectedPreferences(true);
-            }
-          }
-        } catch (error) {
-          setError('Error fetching user preferences: ' + error.message);
-          setSnackbarOpen(true);
-        } finally {
-          setLoading(false);
-        }
-      };
-  
-      fetchUserPreferences();
-    }, []);
-  
-    if (loading) {
-      return <p className='text-white'>Loading...</p>;
+    } catch (error) {
+      setError("Error updating preferences: " + error.message);
+      setSnackbarOpen(true);
     }
-  
-    return (
-      <section className="mb-8">
-        {/* Food Preferences Section */}
-        <div className="bg-white p-8 rounded-md shadow-md"> {/* Increased padding */}
-          <p className="mb-4 font-semibold text-2xl text-gray-800">Please select the type of food you want to eat:</p> {/* Increased font size */}
-          <div className="mt-6 grid grid-cols-3 gap-4">
-            {['Breakfast', 'Lunch', 'Dinner'].map((meal, index) => (
-              <div key={index}>
-                <FormControl fullWidth variant="outlined">
-                  <InputLabel>{meal}</InputLabel>
-                  <Select
-                    {...register(`pref${meal}` as keyof FormValues)} // Explicitly specify the field name
-                    value={temporaryPreferences[`pref${meal}`]}
-                    onChange={(e) => handlePreferenceChange(`pref${meal}`, e.target.value as string)}
-                    defaultValue={"any"}
-                    label={meal}
-                  >
-                    <MenuItem value="any">❓ Any</MenuItem>
-                    <MenuItem value="rice">🍚 Rice</MenuItem>
-                    <MenuItem value="beef">🥩 Beef</MenuItem>
-                    <MenuItem value="chicken">🍗 Chicken</MenuItem>
-                  </Select>
-                </FormControl>
-              </div>
-            ))}
-          </div>
-  
-          <div className="mt-8">
-            <button
-              type="button"
-              onClick={handleSubmit(handleChangePreferences)}
-              className="w-full px-4 py-2 bg-blue-500 text-white rounded-md transition duration-300 hover:bg-blue-600"
-            >
-              Change Preferences
-            </button>
-          </div>
-        </div>
-        {/* Snackbar for displaying errors */}
-        <Snackbar
-          open={snackbarOpen}
-          autoHideDuration={6000}
-          onClose={handleSnackbarClose}
-          message={error || ''}
-        />
-      </section>
-    );
   };
-  
-  export default Preferences;
+
+  useEffect(() => {
+    const fetchUserPreferences = async () => {
+      try {
+        const uid = auth.currentUser?.uid;
+
+        if (uid) {
+          const userDocRef = doc(db, "users", uid);
+          const userDocSnapshot = await getDoc(userDocRef);
+
+          if (userDocSnapshot.exists()) {
+            const userData = userDocSnapshot.data();
+            const { prefBreakfast, prefLunch, prefDinner } = userData || {};
+
+            setTemporaryPreferences({
+              prefBreakfast: prefBreakfast || "",
+              prefLunch: prefLunch || "",
+              prefDinner: prefDinner || "",
+            });
+            setSelectedPreferences([prefBreakfast, prefLunch, prefDinner]);
+            setShowSelectedPreferences(true);
+          }
+        }
+      } catch (error) {
+        setError("Error fetching user preferences: " + error.message);
+        setSnackbarOpen(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserPreferences();
+  }, []);
+
+  if (loading) {
+    return <p className="text-white">Loading...</p>;
+  }
+
+  return (
+    <section className="mb-8">
+      {/* Food Preferences Section */}
+      <div className="bg-white p-8 rounded-md shadow-md">
+        {" "}
+        {/* Increased padding */}
+        <p className="mb-4 font-semibold text-2xl text-gray-800">
+          Please select the type of food you want to eat:
+        </p>{" "}
+        {/* Increased font size */}
+        <div className="mt-6 grid grid-cols-3 gap-4">
+          {["Breakfast", "Lunch", "Dinner"].map((meal, index) => (
+            <div key={index}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel>{meal}</InputLabel>
+                <Select
+                  {...register(`pref${meal}` as keyof FormValues)} // Explicitly specify the field name
+                  value={temporaryPreferences[`pref${meal}`]}
+                  onChange={(e) =>
+                    handlePreferenceChange(
+                      `pref${meal}`,
+                      e.target.value as string
+                    )
+                  }
+                  defaultValue={"any"}
+                  label={meal}
+                >
+                  {meal === "Breakfast" ? (
+                    <>
+                      <MenuItem value="any">❓ Any</MenuItem>
+                      <MenuItem value="any">🥚 Eggs</MenuItem>
+                      <MenuItem value="any">🥛v  Yogurt</MenuItem>
+                      <MenuItem value="any">🥣 Oats</MenuItem>
+                    </>
+                  ) : (
+                    <>
+                      <MenuItem value="any">❓ Any</MenuItem>
+                      <MenuItem value="rice">🍚 Rice</MenuItem>
+                      <MenuItem value="beef">🥩 Beef</MenuItem>
+                      <MenuItem value="chicken">🍗 Chicken</MenuItem>
+                    </>
+                  )}
+                </Select>
+              </FormControl>
+            </div>
+          ))}
+        </div>
+        <div className="mt-8">
+          <button
+            type="button"
+            onClick={handleSubmit(handleChangePreferences)}
+            className="w-full px-4 py-2 bg-blue-500 text-white rounded-md transition duration-300 hover:bg-blue-600"
+          >
+            Change Preferences
+          </button>
+        </div>
+      </div>
+      {/* Snackbar for displaying errors */}
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+        message={error || ""}
+      />
+    </section>
+  );
+};
+
+export default Preferences;
