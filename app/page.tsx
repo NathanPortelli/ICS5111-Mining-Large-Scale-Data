a20026a7e595d57379b0eff333254296ddd331b1
Oleg Grech <oleg.grech.19@um.edu.mt>
2024-01-25 22:11:51 +0100

    fix: authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,104 @@
-'use client'
+"use client";
 
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react'
-import Header from './components/header';
-import { auth } from './firebase';
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Header from "./components/header";
+import { auth } from "./firebase";
+import CircularProgress from "@mui/material/CircularProgress";
+import { User } from "firebase/auth";
 
 export default function Home() {
   const router = useRouter();
-  const isLoggedIn = auth.currentUser !== null; // Check if user is logged in
+  const [user, setUser] = useState<User | null>(null);
 
-  // useEffect(() => {
-  //     // If user is not logged in
-  //   if (!isLoggedIn) {
-  //     router.replace('/credentials');
-  //   }
-  // }, [isLoggedIn, router]);
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      setUser(authUser);
+      if (!authUser) {
+        router.push("/credentials");
+      }
+    });
+  }, [router]);
 
   const handleButtonClick = () => {
-    router.push('/diet');
+    router.push("/diet");
   };
 
   return (
     <main className="flex min-h-screen flex-col">
-      <Header />
-      <h1 className="mt-8 text-4xl sm:text-6xl mb-6 font-semibold text-center text-white">Personalised Diet Recommender</h1>
-      <p className="mt-4 mb-2 text-xl ml-9 mr-9 text-white">Todo: Change the below text to a more fitting introduction</p>
-      <p className="mt-4 mb-8 text-2xl font-semibold ml-9 mr-9 text-white">Welcome to our intelligent diet recommendation web application! Powered by academic panic, this application offers you personalised dietary suggestions based on your BMI, preferences, and similar users’ preferences, time permitting.</p>
+      {user ? (
+        <>
+          <Header />
+          <h1 className="mt-8 text-4xl sm:text-6xl mb-6 font-semibold text-center text-white">
+            Personalised Diet Recommender
+          </h1>
+          <p className="mt-4 mb-2 text-xl ml-9 mr-9 text-white">
+            Todo: Change the below text to a more fitting introduction
+          </p>
+          <p className="mt-4 mb-8 text-2xl font-semibold ml-9 mr-9 text-white">
+            Welcome to our intelligent diet recommendation web application!
+            Powered by academic panic, this application offers you personalised
+            dietary suggestions based on your BMI, preferences, and similar
+            users’ preferences, time permitting.
+          </p>
 
-      <div className="mb-8 ml-9 mr-9 pl-3 pr-3 p-2 border-2 bg-gray-600 rounded-md shadow-md">
-        <p className="text-2xl mb-2 font-semibold text-white">Project Details:</p>
-        <p className="text-xl text-white">Title: Utilising Structured and Unstructured Data for Intelligent Diet Recommendations</p>
-        <p className="text-xl text-white">Created in fulfillment of the requirements for the ICS5111 Mining Large Scale Data assignment by: Nathan Portelli and Oleg Grech.</p>
-        <br />
-        <p className="text-xl font-bold text-white">Email:</p>
-        <p className="text-xl mb-5 text-white">
-          <a className="text-blue-400" href="mailto:nathan.portelli.19@um.edu.mt">nathan.portelli.19@um.edu.mt</a>, <a className="text-blue-400" href="mailto:oleg.grech.19@um.edu.mt">oleg.grech.19@um.edu.mt</a>
-        </p>
-        <p className="text-xl font-bold text-white">Full project code available at:</p>
-        <p className='text-xl'>
-          <a className="text-blue-400" href="https://github.com/NathanPortelli/ICS5111-Mining-Large-Scale-Data">github.com/NathanPortelli/ICS5111-Mining-Large-Scale-Data</a>
-        </p>
-      </div>
+          <div className="mb-8 ml-9 mr-9 pl-3 pr-3 p-2 border-2 bg-gray-600 rounded-md shadow-md">
+            <p className="text-2xl mb-2 font-semibold text-white">
+              Project Details:
+            </p>
+            <p className="text-xl text-white">
+              Title: Utilising Structured and Unstructured Data for Intelligent
+              Diet Recommendations
+            </p>
+            <p className="text-xl text-white">
+              Created in fulfillment of the requirements for the ICS5111 Mining
+              Large Scale Data assignment by: Nathan Portelli and Oleg Grech.
+            </p>
+            <br />
+            <p className="text-xl font-bold text-white">Email:</p>
+            <p className="text-xl mb-5 text-white">
+              <a
+                className="text-blue-400"
+                href="mailto:nathan.portelli.19@um.edu.mt"
+              >
+                nathan.portelli.19@um.edu.mt
+              </a>
+              ,{" "}
+              <a
+                className="text-blue-400"
+                href="mailto:oleg.grech.19@um.edu.mt"
+              >
+                oleg.grech.19@um.edu.mt
+              </a>
+            </p>
+            <p className="text-xl font-bold text-white">
+              Full project code available at:
+            </p>
+            <p className="text-xl">
+              <a
+                className="text-blue-400"
+                href="https://github.com/NathanPortelli/ICS5111-Mining-Large-Scale-Data"
+              >
+                github.com/NathanPortelli/ICS5111-Mining-Large-Scale-Data
+              </a>
+            </p>
+          </div>
 
-      <div className="text-center mb-4">
-        <button
-          type="button"
-          className="text-2xl font-bold rounded-md shadow-md px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue transition duration-300"
-          onClick={handleButtonClick}
-        >
-          Click here to get started
-        </button>
-      </div>
+          <div className="text-center mb-4">
+            <button
+              type="button"
+              className="text-2xl font-bold shadow-md px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue transition duration-300"
+              onClick={handleButtonClick}
+            >
+              Click here to get started
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className="flex min-h-screen flex-col items-center justify-center">
+          <CircularProgress />
+        </div>
+      )}
     </main>
   );
 }
