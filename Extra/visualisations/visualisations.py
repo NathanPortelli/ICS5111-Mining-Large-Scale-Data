067d88ddae4bbe781b2eae28dffd3110ab9f7ade
Oleg Grech <oleg.grech.19@um.edu.mt>
2024-02-03 22:16:52 +0100

    showing both plots at the same time

--- Extra/visualisations/visualisations.py
+++ Extra/visualisations/visualisations.py
@@ -11,20 +11,20 @@ data = response_API.json()
 words = list(data['wordList'])
 vectors = np.array(data['vectors'])
 
-# # t-SNE Visualization
-# tsne = TSNE(n_components=2, random_state=42)
-# vectors_2d = tsne.fit_transform(vectors)
+# t-SNE Visualization
+tsne = TSNE(n_components=2, random_state=42)
+vectors_2d = tsne.fit_transform(vectors)
 
-# # Plotting word vectors in a scatter plot
-# plt.figure(figsize=(10, 8))
-# plt.scatter(vectors_2d[:, 0], vectors_2d[:, 1], marker='o', s=30, alpha=0.6)
+# Plotting word vectors in a scatter plot
+plt.figure(figsize=(10, 8))
+plt.scatter(vectors_2d[:, 0], vectors_2d[:, 1], marker='o', s=30, alpha=0.6)
 
-# # Annotating words for illustration
-# for i, word in enumerate(words):
-#     plt.annotate(word, (vectors_2d[i, 0], vectors_2d[i, 1]), alpha=0.6)
+# Annotating words for illustration
+for i, word in enumerate(words):
+    plt.annotate(word, (vectors_2d[i, 0], vectors_2d[i, 1]), alpha=0.6)
 
-# plt.title('t-SNE Visualization of Word2Vec Embeddings')
-# plt.show()
+plt.title('t-SNE Visualization of Word2Vec Embeddings')
+plt.show()
 
 # PCA Visualization
 pca = PCA(n_components=2)
