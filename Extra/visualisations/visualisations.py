51b0075f35facfe9e88acfce56204c813cd82f56
Oleg Grech <oleg.grech.19@um.edu.mt>
2024-02-03 22:31:34 +0100

    better visualisation

--- Extra/visualisations/visualisations.py
+++ Extra/visualisations/visualisations.py
@@ -11,17 +11,18 @@ data = response_API.json()
 words = list(data['wordList'])
 vectors = np.array(data['vectors'])
 
+# Common parameters
+scatter_params = dict(marker='o', s=50, alpha=0.7)
+
 # t-SNE Visualization
 tsne = TSNE(n_components=2, random_state=42)
 vectors_2d = tsne.fit_transform(vectors)
 
-# Plotting word vectors in a scatter plot
 plt.figure(figsize=(10, 8))
-plt.scatter(vectors_2d[:, 0], vectors_2d[:, 1], marker='o', s=30, alpha=0.6)
+plt.scatter(vectors_2d[:, 0], vectors_2d[:, 1], c='blue', **scatter_params)
 
-# Annotating words for illustration
 for i, word in enumerate(words):
-    plt.annotate(word, (vectors_2d[i, 0], vectors_2d[i, 1]), alpha=0.6)
+    plt.annotate(word, (vectors_2d[i, 0], vectors_2d[i, 1]), alpha=0.7)
 
 plt.title('t-SNE Visualization of Word2Vec Embeddings')
 plt.show()
@@ -30,13 +31,11 @@ plt.show()
 pca = PCA(n_components=2)
 vectors_2d = pca.fit_transform(vectors)
 
-# Plotting word vectors in a scatter plot
 plt.figure(figsize=(10, 8))
-plt.scatter(vectors_2d[:, 0], vectors_2d[:, 1], marker='o', s=30, alpha=0.6)
+plt.scatter(vectors_2d[:, 0], vectors_2d[:, 1], c='red', **scatter_params)
 
-# Annotate some words for illustration
 for i, word in enumerate(words):
-    plt.annotate(word, (vectors_2d[i, 0], vectors_2d[i, 1]), alpha=0.6)
+    plt.annotate(word, (vectors_2d[i, 0], vectors_2d[i, 1]), alpha=0.7)
 
 plt.title('PCA Visualization of Word2Vec Embeddings')
 plt.show()
